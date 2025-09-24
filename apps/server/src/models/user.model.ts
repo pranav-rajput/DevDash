d8aaba182324ec2dee1dc28ffde04165420cdc87
Pranav Rajput <pranavrajput1602@gmail.com>
2025-09-24 22:59:55 +0530

    created user model

---
+++ apps/server/src/models/user.model.ts
@@ -0,0 +1,16 @@
+// apps/server/src/models/user.model.ts
+import { Schema, model, Document } from 'mongoose';
+
+export interface IUser extends Document {
+  githubId: string;
+  username: string;
+  avatarUrl: string;
+}
+
+const UserSchema = new Schema<IUser>({
+  githubId: { type: String, required: true, unique: true },
+  username: { type: String, required: true },
+  avatarUrl: { type: String },
+}, { timestamps: true });
+
+export const User = model<IUser>('User', UserSchema);
