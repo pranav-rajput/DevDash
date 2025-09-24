490c648d4465a4d2e1fc261413a78661a98c27ec
Pranav Rajput <pranav@Pranavs-MacBook-Air-2.local>
2025-09-24 22:38:00 +0530

    final database initial setup

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,8 +1,12 @@
 // apps/server/src/index.ts
 import express from 'express';
+import connectDB from './config/db';
+
+// Connect to database
+connectDB();
 
 const app = express();
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 app.get('/api', (req, res) => {
   res.send('Hello from the Backend!');
